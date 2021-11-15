527c593933dd601ec049f2c11f69f43b4630a83b
Joel Joseph <joel.joseph211@gmail.com>
2021-11-15 10:54:21 +0530

    added functions

---
+++ functions.py
@@ -0,0 +1,13 @@
+# function defination
+
+def function_name(param1, param2):
+    if param1 > param2:
+        return 'param1 is greater'
+
+    else:
+        return 'param2 is greater'
+
+
+# call a function
+result = function_name(param1=5, param2=6)
+print(result)
