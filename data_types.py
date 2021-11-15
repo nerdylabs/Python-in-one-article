b8f4dca474ed28f8830337ed881afca627d6ef79
Joel Joseph <joel.joseph211@gmail.com>
2021-11-15 10:43:37 +0530

    completed data types

---
+++ data_types.py
@@ -0,0 +1,35 @@
+a = 5  # this is int
+int()
+
+b = 5.0
+float()
+
+c = "hey there"
+str()
+
+array = [5, 2, 4, 'j']
+list()
+print(array)
+
+# push a value to the array
+array.append('value')
+print(array)
+
+
+dictionary = {
+    'age': 50,
+    'name': 'Joe',
+    'clear': True
+}
+dict()
+print(dictionary)
+
+
+# add a new key
+dictionary['key'] = 'value'
+print(dictionary)
+
+
+# Check the type of a variable
+print(type(a))
+print(type(array))
--- variables.py
+++ variables.py
@@ -1,3 +1,8 @@
 var = 5  # This is a int type variable
+print(var)
+
 var = 5.0  # This is a float type variable
+print(var)
+
 var = "Hey there"  # This is a string type variable
+print(var)
