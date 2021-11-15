6c5225775a06f242956c155602d2588c5f1b007d
Joel Joseph <joel.joseph211@gmail.com>
2021-11-15 10:32:20 +0530

    added data_types, hello, input_print, variables

---
+++ input_print.py
@@ -0,0 +1,7 @@
+name = input()
+
+print(name)
+
+
+name = input("please enter your name: ")
+print('hello: ', name)
---
+++ variables.py
@@ -0,0 +1,3 @@
+var = 5  # This is a int type variable
+var = 5.0  # This is a float type variable
+var = "Hey there"  # This is a string type variable
