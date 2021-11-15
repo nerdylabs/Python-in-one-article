431c5771aa28076ea112c072e03aa2a9c65d0deb
Joel Joseph <joel.joseph211@gmail.com>
2021-11-15 10:47:04 +0530

    conditionals done

---
+++ conditionals.py
@@ -0,0 +1,13 @@
+a = 5
+b = 6
+if a > b:
+    print("a is greater")
+else:
+    print("a is smaller")
+
+if a > b:
+    print('a is greater')
+elif a == b:
+    print("both are equal")
+else:
+    print("a is smaller")
